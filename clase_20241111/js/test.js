6f4550c16d674e1fcb200d4c9543fdbdaf5f8aa2
crisguilleMM <crisguille+mm@gmail.com>
2024-11-11 20:01:12 -0300

    Add files via upload

---
+++ clase_20241111/js/test.js
@@ -0,0 +1,28 @@
+// Función: crea un nuevo párrafo y lo agrega al final del cuerpo HTML.
+function createParagraph() {
+    let para = document.createElement('p');
+    para.textContent = 'You clicked the button!';
+    document.body.appendChild(para);
+}
+/*
+1. Obtiene referencias de todos los botones de la página en un formato de arreglo.
+2. Recorre todos los botones y agrega un detector de eventos 'click' a cada uno.
+Cuando se presione cualquier botón, se ejecutará la función createParagraph().
+*/
+// const buttons = document.querySelectorAll('button');
+// for (let i = 0; i < buttons.length; i++) {
+//     buttons[i].addEventListener('click', createParagraph);
+// }
+
+/*
+*Agregamos el addEventListener  DOMContentLoaded  
+*para que corra cuando la pagina este cargada
+*/
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll('button');
+    for (let i = 0; i < buttons.length; i++) {
+        console.log(`Loop `+i);//se usa para leer variable 
+        buttons[i].addEventListener('click', createParagraph);
+        console.log(buttons[i],'Tiene : ' +buttons[i].value);
+    }
+});
